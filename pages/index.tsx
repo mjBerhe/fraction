352d6f2e6dbf3703adb3db356419ef701293bae7
mjBerhe <mjberhe123@hotmail.com>
2022-01-08 18:40:43 -0500

    tailwind + prettier + eslint stuffs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-gray-200">hello</div>
-  )
-}
+  const createFraction = () => {};
 
-export default Home
+  return (
+    <div className="min-h-screen">
+      <div className="flex flex-1 h-full items-center border border-yellow-200">
+        hello
+      </div>
+    </div>
+  );
+};
+
+export default Home;
