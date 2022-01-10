507d5815f2a539b700a7bba56b797f6f3bef942e
mjBerhe <mjberhe123@hotmail.com>
2022-01-09 19:38:09 -0500

    created fractionButton component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FunctionComponent, useState } from "react";
 import type { NextPage } from "next";
 
 type Difficulty = "easy" | "hard";
@@ -34,16 +34,12 @@ const Home: NextPage = () => {
     <div className="min-h-screen">
       <div className="flex flex-col h-screen items-center justify-center">
         <button onClick={() => setFractions()}>Get Fractions</button>
-        <div className="flex w-full justify-center space-x-24 p-12">
+        <div className="flex w-full justify-center space-x-12 p-12">
           {fraction1?.length > 0 && (
-            <div>
-              {fraction1[0]} / {fraction1[1]}
-            </div>
+            <FractionButton handleClick={setFractions} fraction={fraction1} />
           )}
           {fraction2?.length > 0 && (
-            <div>
-              {fraction2[0]} / {fraction2[1]}
-            </div>
+            <FractionButton handleClick={setFractions} fraction={fraction2} />
           )}
         </div>
       </div>
@@ -52,3 +48,21 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const FractionButton: FunctionComponent = ({
+  handleClick,
+  fraction,
+}) => {
+  return (
+    <button type="button" onClick={handleClick} className="w-12">
+      <span className="flex flex-col w-full divide-y-2 divide-black">
+        <span className="font-bold text-2xl text-center p-2">
+          {fraction[0]}
+        </span>
+        <span className="font-bold text-2xl text-center p-2">
+          {fraction[1]}
+        </span>
+      </span>
+    </button>
+  );
+};
