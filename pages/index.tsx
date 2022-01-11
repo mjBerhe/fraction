9aa409a4dca21a34328aea6c8a26eee533648def
mjBerhe <mjberhe123@hotmail.com>
2022-01-10 19:49:32 -0500

    no equal fractions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { NextPage } from "next";
 
 import FractionButton from "../components/FractionButton";
 
 type Difficulty = "easy" | "hard";
+
 const getRandomInt = (max: number) => {
   const randomInt = Math.ceil(Math.random() * max);
   return randomInt;
@@ -12,15 +13,18 @@ const getRandomInt = (max: number) => {
 const createFractionPair = (difficulty: Difficulty): number[][] => {
   let d1!: number;
   if (difficulty === "easy") {
-    // denominator of 2-12
-    d1 = getRandomInt(11) + 1;
+    // denominator of 3-12
+    d1 = getRandomInt(10) + 2;
     const n1 = getRandomInt(d1 - 1);
 
+    let n2 = n1;
     let d2 = d1;
-    while (d2 === d1) {
-      d2 = getRandomInt(11) + 1;
+    while (n1 / d1 === n2 / d2) {
+      while (d2 === d1) {
+        d2 = getRandomInt(10) + 2;
+      }
+      n2 = getRandomInt(d2 - 1);
     }
-    const n2 = getRandomInt(d2 - 1);
 
     return [
       [n1, d1],
@@ -53,22 +57,18 @@ const Home: NextPage = () => {
   };
 
   const submitAnswer = (answer: number[]) => {
-    console.log(answer);
+    // console.log(answer);
     if (answer === fraction1) {
       if (fraction1[0] / fraction1[1] > fraction2[0] / fraction2[1]) {
-        console.log("correct!");
         isCorrect();
       } else {
-        console.log("incorrect!");
         isIncorrect();
       }
     }
     if (answer === fraction2) {
       if (fraction2[0] / fraction2[1] > fraction1[0] / fraction1[1]) {
-        console.log("correct!");
         isCorrect();
       } else {
-        console.log("incorrect!");
         isIncorrect();
       }
     }
@@ -76,10 +76,21 @@ const Home: NextPage = () => {
     setFractions();
   };
 
+  useEffect(() => {
+    setFractions();
+  }, []);
+
   return (
     <div className="min-h-screen">
       <div className="flex flex-col h-screen items-center justify-center">
-        <button onClick={() => setFractions()}>Get Fractions</button>
+        {wasCorrect && currentStreak > 0 && (
+          <span className="text-green-600 font-bold text-2xl">
+            Correct! x{currentStreak}
+          </span>
+        )}
+        {!wasCorrect && (
+          <span className="text-red-600 font-bold text-2xl">Incorrect!</span>
+        )}
         <div className="flex w-full justify-center space-x-12 p-12">
           {fraction1?.length > 0 && (
             <FractionButton handleClick={submitAnswer} fraction={fraction1} />
@@ -88,10 +99,6 @@ const Home: NextPage = () => {
             <FractionButton handleClick={submitAnswer} fraction={fraction2} />
           )}
         </div>
-        {wasCorrect && currentStreak > 0 && (
-          <span>Correct! x{currentStreak}</span>
-        )}
-        {!wasCorrect && <span>Incorrect!</span>}
       </div>
     </div>
   );
